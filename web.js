f9de957895f54e93c0312051def69fed5c30037b
miraki <yan.wang314@gmail.com>
2013-07-11 23:48:15 +0200

    update web.js to read from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 var express=require('express');
+var fs=require('fs');
 var app=express.createServer(express.logger());
 
+buf = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
-    response.send('Hello World 2!');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
