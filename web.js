5648581aa1f8d75c95d8b82158423e845c4647c3
miraki <yan.wang314@gmail.com>
2013-07-22 07:59:13 +0200

    styled responsive design

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 var express=require('express');
 var fs=require('fs');
-var app=express.createServer(express.logger());
+var app=express();
 
 buf = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
     response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
